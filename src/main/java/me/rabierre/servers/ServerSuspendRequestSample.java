6ea21ba5384ef3de459fddd973a36bf1c9f0a2d8
rabierre <rabierre@gmail.com>
2013-08-05 15:17:56 +0900

    Add Suspend and Resume Nova Server Request Sample.

---
+++ src/main/java/me/rabierre/servers/ServerSuspendRequestSample.java
@@ -0,0 +1,28 @@
+package me.rabierre.servers;
+
+import com.woorea.openstack.keystone.Keystone;
+import com.woorea.openstack.keystone.model.Access;
+import com.woorea.openstack.keystone.model.authentication.UsernamePassword;
+import com.woorea.openstack.nova.Nova;
+import com.woorea.openstack.nova.api.ServersResource;
+import com.woorea.openstack.nova.model.Servers;
+import me.rabierre.SimpleConfiguration;
+
+public class ServerSuspendRequestSample {
+    public static void main(String[] args) {
+        Keystone keystone = new Keystone(SimpleConfiguration.KEYSTONE_PUBLIC_URL);
+        Access access = keystone.tokens().authenticate(new UsernamePassword(SimpleConfiguration.KEYSTONE_USERNAME, SimpleConfiguration.KEYSTONE_PASSWORD))
+                .withTenantName(SimpleConfiguration.TENANT_ADMIN)
+                .execute();
+
+        //use the token in the following requests
+        keystone.token(access.getToken().getId());
+
+        Nova novaClient = new Nova(SimpleConfiguration.NOVA_COMPUTE_URL.concat("/").concat(access.getToken().getTenant().getId()));
+        novaClient.token(access.getToken().getId());
+
+        Servers servers = novaClient.servers().list(true).execute();
+
+        novaClient.servers().suspend(servers.getList().get(0).getId()).execute();
+    }
+}
