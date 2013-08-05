6ff51ee439d5a8e44c9c00020bff5c8a73ac1db8
rabierre <rabierre@gmail.com>
2013-08-05 15:00:12 +0900

    Add Unpause(Resume) Paused Server Request Sample.

---
+++ src/main/java/me/rabierre/servers/ServerUnpauseRequestSample.java
@@ -0,0 +1,33 @@
+package me.rabierre.servers;
+
+import com.woorea.openstack.keystone.Keystone;
+import com.woorea.openstack.keystone.model.Access;
+import com.woorea.openstack.keystone.model.authentication.UsernamePassword;
+import com.woorea.openstack.nova.Nova;
+import com.woorea.openstack.nova.model.Servers;
+import me.rabierre.SimpleConfiguration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rabierre
+ * Date: 13. 7. 30.
+ * Time: 오후 5:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerUnpauseRequestSample {
+    public static void main(String[] args) {
+        Keystone keystone = new Keystone(SimpleConfiguration.KEYSTONE_PUBLIC_URL);
+        Access access = keystone.tokens().authenticate(new UsernamePassword(SimpleConfiguration.KEYSTONE_USERNAME, SimpleConfiguration.KEYSTONE_PASSWORD))
+                .withTenantName(SimpleConfiguration.TENANT_ADMIN)
+                .execute();
+
+        //use the token in the following requests
+        keystone.token(access.getToken().getId());
+
+        Nova novaClient = new Nova(SimpleConfiguration.NOVA_COMPUTE_URL.concat("/").concat(access.getToken().getTenant().getId()));
+        novaClient.token(access.getToken().getId());
+
+        Servers servers = novaClient.servers().list(true).execute();
+        novaClient.servers().unpause(servers.getList().get(0).getId()).execute();
+    }
+}
